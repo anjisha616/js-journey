7200879b56ae21156a628b13076590156d717827
anjisha616 <punangisha@gmail.com>
2025-12-18 16:43:26 +0545

    day1

---
+++ script.js
@@ -0,0 +1,32 @@
+// window.alert(`Hey!! How's going your learning?`)
+// console.log(`Hello World`);
+
+
+// document.getElementById("myh1").textContent= `Hello Gyzs`;
+// document.getElementById("myp").textContent = `New to JS!!!!!`;
+//  --------
+
+// variable=container that store CSSMathValue.behaves as it were value it contain.
+// let age = 19;
+// let gpa = 3.5;
+// let name = "anjisha";
+// let boolean="true";
+
+// console.log(`the age is ${age}yrs old.`);
+// console.log(`the gpa is ${gpa}.`);
+// console.log(`the name is ${name}`);
+// console.log(`the name is ${boolean}`);
+
+
+// ---Arithmetic----
+let x=23;
+
+// x=x+1;   //addition
+// x=x-1;   //sub
+// x=x*2;  //mul
+// x=x/2;   //div
+// x= x**2;      //exponent
+x=x%10;        //modulus 
+
+console.log(x);
+
